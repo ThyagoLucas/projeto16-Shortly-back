fa2ebbc99fffda995cf53040a13029fb25e6a007
pedro imbrech <elprogramadorpedro@gmail.com>
2022-06-24 11:46:33 -0300

    rev

--- routers/signinRouter.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { login } from '../controllers/signinController.js';
-import { verifyUserAndPass } from '../middlewares/verifiesMidds.js';
-
-const signinRouter = Router();
-
-signinRouter.post('/signin', verifyUserAndPass, login );
-
-export default signinRouter;
